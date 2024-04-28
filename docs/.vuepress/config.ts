895c44b39c481f84c078094b24be87be61a3d4d2
feiniao-zlh <742738858@qq.com>
2024-04-28 18:02:50 +0800

    二分

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -65,6 +65,10 @@ export default defineConfig4CustomTheme<VdoingThemeConfig>({
                 ],
             },
             {
+                text: '算法',
+                link: '/pages/23e810/', //目录页链接，此处link是vdoing主题新增的配置项，有二级导航时，可以点击一级导航跳到目录页
+            },
+            {
                 text: '更多',
                 link: '/more/',
                 items: [
